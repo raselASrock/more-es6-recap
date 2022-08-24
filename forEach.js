a63d79cff86d2fde8c7582c52f848202ce9fe0e1
raselASrock <mdraselm413@gmail.com>
2022-08-25 00:13:05 +0600

    forEach-added

---
+++ forEach.js
@@ -0,0 +1,14 @@
+
+
+const friends = ['Tom Hanks', 'Tom cruise', 'Tom Brady', 'Tom Solaiman'];
+
+friends.forEach(friend => console.log(friend));
+
+
+const products = [
+    {id:1001, name: 'Laptop', price: 45000},
+    {id:1002, name: 'Mobile', price: 15000},
+    {id:1003, name: 'Watch', price: 35000},
+    {id:1004, name: 'Tablet', price: 23000},
+    {id:1005, name: 'Laptop', price: 45000},
+];
