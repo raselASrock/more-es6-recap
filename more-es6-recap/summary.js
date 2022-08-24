a196511f914e08d6a38414455f1e9cf7869a1ffd
raselASrock <mdraselm413@gmail.com>
2022-08-25 03:13:40 +0600

    summary-added

---
+++ more-es6-recap/summary.js
@@ -0,0 +1,8 @@
+/* 
+1. Optional Chaining ( student.marks.math )
+2. map, filter, find (Must, Very Important)
+3. forEach(Optional), reduce(Advance)
+4. Class (How it works?)
+5. Inheritance, Super (Optional)
+6.Prototypical Inheritance (Must , Imoortant)
+*/
